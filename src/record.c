e7b429052f031bdf48e29a0a87c47e0ab01bd9fd
Rayon-Richter Malou <rayon.richter.malou@gmail.com>
2019-12-12 19:09:15 +0100

    add a functionnality to disable logging and a modify errno reset at the end of the function

--- src/record.c
+++ src/record.c
@@ -1,8 +1,64 @@
 #include "record.h"
 #include "config.h"
 
+extern int record_enabled[5];
+void enable_record(int info, int debug, int warning, int error, int fatalerror){
+	if (info){
+		record_enabled[RECORD_LOG_INFO] = 1;
+	}
+	else {
+		record_enabled[RECORD_LOG_INFO] = 0;
+	}
+	if (debug){
+		record_enabled[RECORD_LOG_DEBUG] = 1;
+	}
+	else {
+		record_enabled[RECORD_LOG_DEBUG] = 0;
+	}
+	if (warning){
+		record_enabled[RECORD_LOG_WARNING] = 1;
+	}
+	else {
+		record_enabled[RECORD_LOG_WARNING] = 0;
+	}
+	if (error){
+		record_enabled[RECORD_LOG_ERROR] = 1;
+	}
+	else {
+		record_enabled[RECORD_LOG_ERROR] = 0;
+	}
+	if (fatalerror){
+		record_enabled[RECORD_LOG_FATALERROR] = 1;
+	}
+	else {
+		record_enabled[RECORD_LOG_FATALERROR] = 0;
+	}
+	return;
+}
+void enable_record_all(int boolean){
+	if (boolean){
+		record_enabled[0] = 1;
+		record_enabled[1] = 1;
+		record_enabled[2] = 1;
+		record_enabled[3] = 1;
+		record_enabled[4] = 1;
+		return;
+	}
+	record_enabled[0] = 0;
+	record_enabled[1] = 0;
+	record_enabled[2] = 0;
+	record_enabled[3] = 0;
+	record_enabled[4] = 0;
+	return;
+}
+
 void record(char logType, char * title, char * message, char checkErrno, char * FILE, int LINE){
-	
+	if (logType < 0 || logType > 4){
+		return;
+	}
+	if (record_enabled[logType] == 0){
+		return;
+	}
 	int trueErrno = errno;
 	time_t logTimeValue = time(NULL);
 	char logErrorType[16], openColor[10];
@@ -47,7 +103,7 @@ void record(char logType, char * title, char * message, char checkErrno, char * 
 	if (checkErrno && trueErrno != 0){
 		dprintf(outFile, "\t> errno is %d\n\t> %s\n" , trueErrno, strerror(trueErrno));
 	}
-	errno = trueErrno;
 	fflush(stdout);
 	fflush(stderr);
+	errno = trueErrno;
 }
--- src/record.h
+++ src/record.h
@@ -7,13 +7,16 @@
 #include <string.h>
 #include <errno.h>
 
+int record_enabled [5] = {1, 1, 1, 1, 1};
+void enable_record(int info, int debug, int warning, int error, int fatalerror);
+void enable_record_all(int boolean);
 void record(char logType, char * title, char * message, char checkErrno, char * FILE, int LINE);
 
-#define RECORD_LOG_INFO 1 /* stdout */
-#define RECORD_LOG_DEBUG 2 /* stdout */
-#define RECORD_LOG_WARNING 3 /* stderr */
-#define RECORD_LOG_ERROR 4 /* stderr */
-#define RECORD_LOG_FATALERROR 5 /* stderr */
+#define RECORD_LOG_INFO 0 /* stdout */
+#define RECORD_LOG_DEBUG 1 /* stdout */
+#define RECORD_LOG_WARNING 2 /* stderr */
+#define RECORD_LOG_ERROR 3 /* stderr */
+#define RECORD_LOG_FATALERROR 4 /* stderr */
 
 #define RECORD_NO_CHECK_ERRNO 0
 #define RECORD_CHECK_ERRNO 1
